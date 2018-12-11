511081c55cfe4de2cfca5a703f7cb20446f8f7f0
Shashikant Sharma <shashi@prodiodesignworks.in>
2018-12-11 21:28:28 +0530

    new api updated

--- common/models/ezpay-payees.js
+++ common/models/ezpay-payees.js
@@ -350,6 +350,45 @@ module.exports = function(Ezpaypayees) {
     }
 
 
+        Ezpaypayees.remoteMethod(
+        'getPayerProfileByEmail', {
+            http: {
+                verb: 'post'
+            },
+            description: ["Add Payee"],
+            accepts: [{
+                arg: 'payerEmail',
+                type: 'string',
+                required: true,
+                http: {
+                    source: 'query'
+                }
+            }, ],
+            returns: {
+                type: 'object',
+                root: true
+            }
+        }
+    );
+
+    Ezpaypayees.getPayerProfileByEmail = (payerEmail, cb) => {
+        Ezpaypayees.findOne({"email":payerEmail}).then(payeeObj => {
+            if (isValidObject(payeeObj)) {
+                cb(null, payeeObj);
+            } else {
+                cb(new HttpErrors.InternalServerError('Invalid Payee ID.', {
+                    expose: false
+                }));
+            }
+        }).catch(error => {
+            cb(new HttpErrors.InternalServerError('Server Error', {
+                expose: false
+            }));
+        });
+    }
+
+
+
     Ezpaypayees.remoteMethod(
         'getPayerProfile', {
             http: {
