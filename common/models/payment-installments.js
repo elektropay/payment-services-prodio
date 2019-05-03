e664a6cd5310d9313e26fb3627123a4a612cbc7b
Shashikant Sharma <shashi@prodiodesignworks.in>
2019-05-03 13:38:20 +0530

    enable/disable installments feature apis added

--- common/models/payment-installments.js
+++ common/models/payment-installments.js
@@ -108,7 +108,7 @@ module.exports = function(Paymentinstallments) {
                 arg: 'req',
                 type: 'object',
                 http: ctx => {
-                    return ctx.res;
+                    return ctx.req;
                 }
             }
             ],
@@ -132,7 +132,7 @@ module.exports = function(Paymentinstallments) {
 			            "paymentStatus":installmentData["paymentStatus"],
 			            "metaData":installmentData["metaData"]
 			        };
-			        console.log(updateJson)
+			        //console.log(updateJson)
     			installInfo.updateAttributes(updateJson).then(updateInfo=>{
     				//TODO: Update agenda
     				schedular.rescheduleEvent(installmentData["installmentId"],updateJson);
@@ -148,6 +148,87 @@ module.exports = function(Paymentinstallments) {
                 expose: false
             }));
     	})
+    }
+
+
+    Paymentinstallments.remoteMethod(
+        'editMultipleInstallments', {
+            http: {
+                verb: 'post'
+            },
+            description: ["This request will add new installment withing the transaction"],
+            accepts: [
+            {
+                arg: 'installmentData',
+                type: 'object',
+                required: true,
+                http: {
+                    source: 'body'
+                }
+            },
+            {
+                arg: 'req',
+                type: 'object',
+                http: ctx => {
+                    return ctx.req;
+                }
+            }
+            ],
+            returns: {
+                type: 'object',
+                root: true
+            }
+        }
+    );
+
+    Paymentinstallments.editMultipleInstallments = (installmentData,req, cb) => {
+        if (!isNull(installmentData["meta"])) { installmentData = installmentData["meta"]; }
+        let approvedFor = []; let failedFor = [];
+
+        Paymentinstallments.app.models.ezpayPaymentTransactions.findById(installmentData["transactionId"]).then(transInfo=>{
+            if(isValidObject(transInfo)){
+
+                async.each(installmentData["installments"],function(item,clb){
+                    Paymentinstallments.findById(item["installmentId"]).then(installInfo=>{
+                        if(isValidObject(installInfo)){
+                            let updateJson = {
+                                    "refTransactionId":installInfo["transactionId"],
+                                    "installmentLabel": item["installmentLabel"],
+                                    "amount":item["amount"],
+                                    "dueDate":new Date(item["dueDate"]+" 02:00"),
+                                    "paymentStatus":item["paymentStatus"],
+                                    "metaData":item["metaData"]
+                                };
+                                //console.log(updateJson)
+                            installInfo.updateAttributes(updateJson).then(updateInfo=>{
+                                //TODO: Update agenda
+                                approvedFor.push(item["installmentId"]);
+                                schedular.rescheduleEvent(item["installmentId"],updateJson);
+                                clb();
+                            });
+                        }else{
+                            failedFor.push(item["installmentId"]);
+                            clb();
+                        }
+                    }).catch(err=>{
+                        failedFor.push(item["installmentId"]);
+                        clb();
+                    });
+                },function(){
+                    cb(null,{"success":true,"approvedFor":approvedFor,"failedFor":failedFor});
+                });
+                
+            }else{
+                cb(new HttpErrors.InternalServerError('Invalid transaction Id.', {
+                     expose: false
+                }));
+            }
+        }).catch(err=>{
+            cb(new HttpErrors.InternalServerError('Error while fetching transaction info.', {
+                        expose: false
+            }));
+        });
+        
     }
 
 
@@ -170,7 +251,7 @@ module.exports = function(Paymentinstallments) {
                 arg: 'req',
                 type: 'object',
                 http: ctx => {
-                    return ctx.res;
+                    return ctx.req;
                 }
             }
             ],
@@ -184,27 +265,175 @@ module.exports = function(Paymentinstallments) {
     Paymentinstallments.removeInstallment = (installmentData,req, cb) => {
     	if (!isNull(installmentData["meta"])) { installmentData = installmentData["meta"]; }
 
-    	Paymentinstallments.findById(installmentData["installmentId"]).then(installInfo=>{
-    		if(isValidObject(installInfo)){
-    			Paymentinstallments.removeById(installmentData["installmentId"]).then(data=>{
-    				//TODO: cancel event
-    				schedular.cancelEvent(installmentData["installmentId"]);
-    				cb(null,{"success":true,"data": data});
-    			}).catch(err=>{
-    				cb(new HttpErrors.InternalServerError('Error while removing installment.', {
-		                expose: false
-		            }));
-    			});
-    		}else{
-    			cb(new HttpErrors.InternalServerError('Installment info not exists.', {
-	                expose: false
-	            }));
-    		}
-    	}).catch(err=>{
-    		cb(new HttpErrors.InternalServerError('Error while fetching installment info.', {
+        Paymentinstallments.app.models.ezpayPaymentTransactions.findById(installmentData["transactionId"]).then(transInfo=>{
+            if(isValidObject(transInfo)){
+                Paymentinstallments.findById(installmentData["installmentId"]).then(installInfo=>{
+                    if(isValidObject(installInfo)){
+                        Paymentinstallments.removeById(installmentData["installmentId"]).then(data=>{
+                            //TODO: cancel event
+                            schedular.cancelEvent(installmentData["installmentId"]);
+                            cb(null,{"success":true,"data": data});
+                        }).catch(err=>{
+                            cb(new HttpErrors.InternalServerError('Error while removing installment.', {
+                                expose: false
+                            }));
+                        });
+                    }else{
+                        cb(new HttpErrors.InternalServerError('Installment info not exists.', {
+                            expose: false
+                        }));
+                    }
+                }).catch(err=>{
+                    cb(new HttpErrors.InternalServerError('Error while fetching installment info.', {
+                        expose: false
+                    }));
+                })
+            }else{
+                cb(new HttpErrors.InternalServerError('Invalid transaction Id.', {
+                     expose: false
+                }));
+            }
+        }).catch(err=>{
+            cb(new HttpErrors.InternalServerError('Error while fetching transaction info.', {
+                        expose: false
+            }));
+        })
+    	
+    }
+
+
+    Paymentinstallments.remoteMethod(
+        'disbleInstallments', {
+            http: {
+                verb: 'post'
+            },
+            description: ["This request will add new installment withing the transaction"],
+            accepts: [
+            {
+                arg: 'installmentData',
+                type: 'object',
+                required: true,
+                http: {
+                    source: 'body'
+                }
+            },
+            {
+                arg: 'req',
+                type: 'object',
+                http: ctx => {
+                    return ctx.req;
+                }
+            }
+            ],
+            returns: {
+                type: 'object',
+                root: true
+            }
+        }
+    );
+
+    Paymentinstallments.disbleInstallments = (installmentData,req, cb) => {
+        if (!isNull(installmentData["meta"])) { installmentData = installmentData["meta"]; }
+        let returnObj = {"success":true,"data":{"approvedFor":[],"failedFor":[]}};
+        Paymentinstallments.app.models.ezpayPaymentTransactions.findById(installmentData["transactionId"]).then(transInfo=>{
+            if(isValidObject(transInfo)){
+                async.each(installmentData["installmentIds"],function(item,clb){
+                    Paymentinstallments.findById(item).then(installInfo=>{
+                        if(isValidObject(installInfo)){
+                            installInfo.updateAttributes({"isActive":false}).then(updateInfo=>{
+                                returnObj["data"]["approvedFor"].push(item);
+                                schedular.cancelEvent(item);
+                                clb();
+                            });
+                        }else{
+                            returnObj["data"]["failedFor"].push(item);
+                            clb();
+                        }
+                    }).catch(err=>{
+                        returnObj["data"]["failedFor"].push(item);
+                        clb();
+                    });
+                },function(){
+                    cb(null,returnObj);
+                })
+            }else{
+                cb(new HttpErrors.InternalServerError('Invalid Transaction Id.', {
+                    expose: false
+                }));
+            }
+        }).catch(err=>{
+            cb(new HttpErrors.InternalServerError('Error while fetching transaction info.', {
                 expose: false
             }));
-    	})
+        });
     }
+
+
+    Paymentinstallments.remoteMethod(
+        'enableInstallments', {
+            http: {
+                verb: 'post'
+            },
+            description: ["This request will add new installment withing the transaction"],
+            accepts: [
+            {
+                arg: 'installmentData',
+                type: 'object',
+                required: true,
+                http: {
+                    source: 'body'
+                }
+            },
+            {
+                arg: 'req',
+                type: 'object',
+                http: ctx => {
+                    return ctx.req;
+                }
+            }
+            ],
+            returns: {
+                type: 'object',
+                root: true
+            }
+        }
+    );
+
+    Paymentinstallments.enableInstallments = (installmentData,req, cb) => {
+        if (!isNull(installmentData["meta"])) { installmentData = installmentData["meta"]; }
+        let returnObj = {"success":true,"data":{"approvedFor":[],"failedFor":[]}};
+        Paymentinstallments.app.models.ezpayPaymentTransactions.findById(installmentData["transactionId"]).then(transInfo=>{
+            if(isValidObject(transInfo)){
+                async.each(installmentData["installmentIds"],function(item,clb){
+                    Paymentinstallments.findById(item).then(installInfo=>{
+                        if(isValidObject(installInfo)){
+                            installInfo.updateAttributes({"isActive":true}).then(updateInfo=>{
+                                returnObj["data"]["approvedFor"].push(item);
+                                funScheduleJob(installInfo,installmentData["hostBaseURL"],req);
+                                clb();
+                            });
+                        }else{
+                            returnObj["data"]["failedFor"].push(item);
+                            clb();
+                        }
+                    }).catch(err=>{
+                        returnObj["data"]["failedFor"].push(item);
+                        clb();
+                    });
+                },function(){
+                    cb(null,returnObj);
+                })
+            }else{
+                cb(new HttpErrors.InternalServerError('Invalid Transaction Id.', {
+                    expose: false
+                }));
+            }
+        }).catch(err=>{
+            cb(new HttpErrors.InternalServerError('Error while fetching transaction info.', {
+                expose: false
+            }));
+        });
+    }
+
 
 };
